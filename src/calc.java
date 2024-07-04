f3e2272862cdeec2dc747da28a1aed6c1d5dedd2
Joel Jaquez <jaq23369@uvg.edu.gt>
2024-07-04 12:03:18 -0600

    Actualizacion

--- src/calc.java
+++ src/calc.java
@@ -1,1 +1,17 @@
-# src
+public class calc{
+
+    //Metodo para sumar 2 numeros
+    public int suma(int n1, int n2){
+        return n1 + n2;
+    }
+
+    //Metodo para restar 2 numeros
+    public int resta(int n1, int n2){
+        return n1 - n2;
+    }
+
+    //Metodo para multiplicar 2 numeros
+    public int multiplicacion(int n1, int n2){
+        return n1 * n2;
+    }
+}
